ff76bf4c6c2c690c944c94821b1668ff9741fc39
Mikhail <dubovikov94@gmail.com>
2025-04-04 01:05:16 +0300

    Configure the pinch-to-zoom state

---
+++ features/picture_detail_screen/src/main/java/com/mdubovikov/picture_detail_screen/presentation/PictureDetailScreen.kt
@@ -0,0 +1,101 @@
+package com.mdubovikov.picture_detail_screen.presentation
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.mdubovikov.ui.R
+import com.mdubovikov.ui.TopBar
+
+@Composable
+fun PictureDetailScreen(
+    pictureUrl: String?,
+    onBackClick: () -> Unit
+) {
+
+    var scale by remember { mutableFloatStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+
+    val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
+        scale = (scale * zoomChange).coerceIn(1f, 5f)
+
+        offset = Offset(
+            x = (offset.x + scale * panChange.x),
+            y = (offset.y + scale * panChange.y)
+        )
+    }
+
+    Scaffold(
+        topBar = {
+            TopBar(
+                screenName = stringResource(R.string.picture),
+                onBackClick = { onBackClick() }
+            )
+        }
+    ) { padding ->
+
+        Box(
+            modifier = Modifier
+                .padding(padding)
+                .pointerInput(Unit) {
+                    detectTapGestures(onDoubleTap = {
+                        scale = if (scale > 1f) 1f else 2f
+                        offset = Offset.Zero
+                    })
+                }
+        ) {
+            if (pictureUrl != null) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(pictureUrl.replaceQuality("b"))
+                        .crossfade(true)
+                        .build(),
+                    contentScale = ContentScale.Fit,
+                    filterQuality = FilterQuality.None,
+                    contentDescription = stringResource(R.string.image),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .graphicsLayer(
+                            scaleX = scale,
+                            scaleY = scale,
+                            translationX = offset.x,
+                            translationY = offset.y
+                        )
+                        .transformable(state)
+                )
+            } else {
+                Text(
+                    text = stringResource(R.string.error),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .alpha(0.5f)
+                )
+            }
+        }
+    }
+}
+
+private fun String.replaceQuality(quality: String): String =
+    this.replace(Regex("_\\w\\.jpg$"), "_$quality.jpg")
